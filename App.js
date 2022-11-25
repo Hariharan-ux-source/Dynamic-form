7c7d76df50edfba09564fe7c170cee99e56a3b7d
Hariharan-ux-source <118712712+Hariharan-ux-source@users.noreply.github.com>
2022-11-25 17:07:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Formm from './form1';
+import ValidatingForm from './form1';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidatingForm/>
+    </div>
+  );
+}
+
+export default App;
